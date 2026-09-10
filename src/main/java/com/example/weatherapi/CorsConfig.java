a8b5f188f88459e7abb2a9d7302fdb89c47be38b
Thinal2004 <thinalprabasha@gmail.com>
2026-09-10 23:34:09 +0530

    Add CORS

---
+++ src/main/java/com/example/weatherapi/CorsConfig.java
@@ -0,0 +1,15 @@
+package com.example.weatherapi;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Applies this rule to all your API endpoints
+                .allowedOrigins("*") // Allows any frontend to connect (useful for dev)
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
+}
